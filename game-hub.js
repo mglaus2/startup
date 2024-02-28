acb348a72b994d2b0ce4c78502d41057f872bb8c
mglaus2 <matthew.glaus@gmail.com>
2024-02-28 15:02:39 -0700

    Login and Connection Support Finished

---
+++ game-hub.js
@@ -0,0 +1,17 @@
+function handleFormSubmission() {
+    const gameIDEl = document.getElementById('gameID');
+
+    if (!gameIDEl.value) {
+        alert("Please enter a GameID");
+        return;
+    }
+
+    localStorage.setItem("gameID", gameIDEl.value);
+
+    // This is where the websocket would get the opponents name and put info into database
+    const opponentName = "Opponent";
+    localStorage.setItem("opponentName", opponentName);
+    alert(`Connected with ${opponentName} with GameID: ${gameIDEl.value}`);
+
+    window.location.href = window.location.href = "play.html";
+}
--- login.js
+++ login.js
@@ -1,5 +1,15 @@
 function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("username", nameEl.value);
-    window.location.href = "play.html";
+    const usernameEl = document.getElementById('name');
+    const passwordEl = document.getElementById('password');
+
+    if(!usernameEl.value || !passwordEl.value) {
+        alert("Please enter valid username or password");
+        return;
+    }
+    
+    // validate and/or store username and encoded password into the database
+    localStorage.setItem("username", usernameEl.value);
+    localStorage.setItem("password", passwordEl.value);
+
+    window.location.href = "game-hub.html";
 }
