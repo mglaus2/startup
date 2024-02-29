9e52c18874636f37ea6b915e16d2764d5c002040
mglaus2 <matthew.glaus@gmail.com>
2024-02-28 17:00:45 -0700

    Leaderboard Implemented

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,54 @@
 const playerNameEl = document.getElementById('username'); 
-playerNameEl.textContent = this.getPlayerName() + '\'s Leaderboard';
+const username = localStorage.getItem('username') ?? 'Mystery Player';
+playerNameEl.textContent = username + '\'s Leaderboard';
 
-function getPlayerName() {
-    return localStorage.getItem('username') ?? 'Mystery Player';
-}
+let totalWins = 0;
+let totalLosses = 0;
+
+function loadScores() {
+    let hasRecord = false;
+    let records = [];
+    const recordsText = localStorage.getItem('gameRecords');
+    if (recordsText) {
+      records = JSON.parse(recordsText);
+    }
+  
+    const tableBodyEl = document.querySelector('#records');
+  
+    if (records.length) {
+        console.log("IN RECORDS");
+        for (const [i, record] of records.entries()) {
+            if(record.username === username) {
+                hasRecord = true;
+                const opponentTdEl = document.createElement('td');
+                const winsTdEl = document.createElement('td');
+                const lossesTdEl = document.createElement('td');
+        
+                opponentTdEl.textContent = record.opponent;
+                winsTdEl.textContent = record.wins;
+                lossesTdEl.textContent = record.losses;
+
+                totalWins += record.wins;
+                totalLosses += record.losses;
+        
+                const rowEl = document.createElement('tr');
+                rowEl.appendChild(opponentTdEl);
+                rowEl.appendChild(winsTdEl);
+                rowEl.appendChild(lossesTdEl);
+        
+                tableBodyEl.appendChild(rowEl);
+            }
+        }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Play a game to have a leaderboard!</td></tr>';
+    }
+
+    if(!hasRecord) {
+        tableBodyEl.innerHTML = '<tr><td colSpan=4>Play a game to have a leaderboard!</td></tr>';
+    }
+
+    const overallRecordEl = document.getElementById('overall-record');
+    overallRecordEl.textContent = `Overall Record: ${totalWins}-${totalLosses}`;
+  }
+  
+  loadScores();
