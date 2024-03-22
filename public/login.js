8cc2bd934fb1fbbd6d8bb4e0131da4cdd41a48c7
mglaus2 <matthew.glaus@gmail.com>
2024-03-22 10:59:15 -0600

    Create User, Login, and Logout Frontend Implemented

--- public/login.js
+++ public/login.js
@@ -1,36 +1,83 @@
-async function login() {
+document.addEventListener('DOMContentLoaded', async () => {
+    const username = localStorage.getItem('username');
+    console.log('Username: ', username);
+    if (username) {
+        document.getElementById('playerName').textContent = username;
+        setDisplay('loginControls', 'none');
+        setDisplay('playControls', 'block');
+        const h1Element = document.querySelector('main h1');
+        h1Element.style.marginBottom = '1vh';
+    } else {
+        setDisplay('loginControls', 'block');
+        setDisplay('playControls', 'none');
+        const h1Element = document.querySelector('main h1');
+        h1Element.style.marginBottom = '3vh';
+    }
+});
+
+
+
+async function loginUser() {
+    loginOrCreate('/api/auth/login');
+}
+
+async function createUser() {
+    loginOrCreate('/api/auth/create');
+}
+
+async function loginOrCreate(endpoint) {
     const usernameEl = document.getElementById('name');
     const passwordEl = document.getElementById('password');
 
     if(!usernameEl.value || !passwordEl.value) {
-        alert("Please enter valid username or password");
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: Please enter valid username or password`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
         return;
     }
-    
+
     const data = {
-        username: usernameEl.value,
+        email: usernameEl.value,
+        password: passwordEl.value,
     };
 
-    try {
-        const response = await fetch('/api/saveUsername', {
-            method: 'POST',
-            headers: {'content-type': 'application/json'},
-            body: JSON.stringify(data),
-        });
-        
-        const returnData = await response.json();
-        const message = returnData.message;
-        console.log(message);
-        if(message === 'Username saved successfully') {
-            alert('Server Success for Saving Username');
-            localStorage.setItem("username", usernameEl.value);
-            localStorage.setItem("password", passwordEl.value);
-        }
-    } catch {
-        alert('Server Error with Saving Username');
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
         localStorage.setItem("username", usernameEl.value);
-        localStorage.setItem("password", passwordEl.value);
+        window.location.href = "game-hub.html";
+    } else {
+        const body = await response.json();
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
     }
-    
-    window.location.href = "game-hub.html";
+}
+
+async function goToGameHub() {
+    window.location.href = 'game-hub.html';
+}
+
+async function logout() {
+    localStorage.removeItem('username');
+    fetch('/api/auth/logout', {
+        method: 'DELETE',
+    }).then(() => (window.location.href = '/'));
+}
+
+function setDisplay(controlId, display) {
+    console.log(controlId);
+    const playControlEl = document.getElementById(`${controlId}`);
+    console.log('IN SET DISPLAY');
+    console.log(playControlEl);
+    if (playControlEl) {
+        console.log('Setting element display');
+        playControlEl.style.display = display;
+    }
 }
