c58ecb8964f010b7df6820b0fd77c2a64927f09d
mglaus2 <matthew.glaus@gmail.com>
2024-04-16 19:13:05 -0600

    Router Created

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Leaderboard } from './leaderboard/leaderboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
   return (
-    <div className="body">
-      <header className="container-fluid">
-      <nav className="navbar">
-        <a className="navbar-brand" href="index.html">Battleship</a>
-        <menu className="navbar-nav">
-          <li className="nav-item"><a className="nav-link" href="index.html">Home</a></li>
-          <li className="nav-item"><a className="nav-link" href="play.html">Current Game</a></li>
-          <li className="nav-item"><a className="nav-link active" href="leaderboard.html">Leaderboard</a></li>
-        </menu>
-      </nav>
-    </header>
+    <BrowserRouter>
+      <div className="body">
+        <header className="container-fluid">
+          <nav className="navbar">
+            <NavLink className="navbar-brand" to="">Battleship</NavLink>
+            <menu className="navbar-nav">
+              <li className="nav-item"><NavLink className="nav-link" to="">Home</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="play">Current Game</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link active" to="leaderboard">Leaderboard</NavLink></li>
+            </menu>
+          </nav>
+        </header>
 
-    <main>App components go here</main>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/play' element={<Play />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-    <footer className="text-dark">
-      <span className="text-reset">By: Matthew Glaus</span>
-      <a href="https://github.com/mglaus2/startup">My Startup Github Repo</a>
-    </footer>
-    </div>
+        <footer className="text-dark">
+          <span className="text-reset">By: Matthew Glaus</span>
+          <a href="https://github.com/mglaus2/startup">My Startup Github Repo</a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
